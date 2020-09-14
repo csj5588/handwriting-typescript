5d56f2dc01a85e846c111703f23f4d0c6dc9313e
cuishijie <cuishijie@inke.cn>
2020-09-14 13:37:39 +0800

    add: unknow and any

--- basetype/index.js
+++ basetype/index.js
@@ -16,12 +16,13 @@ var list2 = [1, 2, 3]; // Array<number>泛型语法。
 // enum
 // 通过观察数字枚举和字符串枚举的编译结果，我们可以知道数字枚举除了支持
 // 从成员名称到成员值 的普通映射之外，它还支持 从成员值到成员名称 的反向映射：
+// 枚举类型常用场景为 常量枚举 - 对照表
 var Direction;
 (function (Direction) {
-    Direction[Direction["NORTH"] = 3] = "NORTH";
-    Direction["SOUTH"] = "SOUTH";
-    Direction[Direction["EAST"] = void 0] = "EAST";
-    Direction[Direction["WEST"] = void 0] = "WEST";
+    Direction[Direction["NORTH"] = 0] = "NORTH";
+    Direction[Direction["SOUTH"] = 1] = "SOUTH";
+    Direction[Direction["EAST"] = 2] = "EAST";
+    Direction[Direction["WEST"] = 3] = "WEST";
 })(Direction || (Direction = {}));
 var dir = Direction.NORTH;
 console.log(dir);
