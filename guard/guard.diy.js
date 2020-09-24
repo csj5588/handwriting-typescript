175ce5abbbb257563eca2ce2cc0a06a86f200405
cuishijie <cuishijie@inke.cn>
2020-09-24 10:17:48 +0800

    guard.diy

---
+++ guard/guard.diy.js
@@ -0,0 +1,8 @@
+// 自定义类型保护的类型谓词
+function isNumber(x) {
+    return typeof x === "number";
+}
+function isString(x) {
+    return typeof x === "string";
+}
+isNumber('lalla');
---
+++ guard/guard.diy.ts
@@ -0,0 +1,11 @@
+// 自定义类型保护的类型谓词
+
+function isNumber(x: any): x is number {
+  return typeof x === "number";
+}
+
+function isString(x: any): x is string {
+  return typeof x === "string";
+}
+
+isNumber('lalla')
