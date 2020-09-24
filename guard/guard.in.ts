a7b71db3e47c6e1d0d6c4ac22f4a6a6cb05ca870
cuishijie <cuishijie@inke.cn>
2020-09-24 09:32:25 +0800

    guard.in.ts

---
+++ guard/guard.in.ts
@@ -0,0 +1,23 @@
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfomation(emp: UnknownEmployee) {
+  console.log("name" + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInfomation({ name: 'cuishijie', startDate: new Date() })
