dae96020222a672c04ca1bde3f750e10bce11843
cuishijie <cuishijie@inke.cn>
2020-10-10 14:00:41 +0800

    type alias ts

--- type/type.ts
+++ type/type.ts
@@ -1,4 +1,5 @@
 /**
+ * 类型别名（type alias）
  * 这里解释一下type 与interface的区别，interface是表述一个函数 类，对象的形状。
  * 
  */
@@ -17,5 +18,6 @@ type User2 = {
   age: number
 }
 
-// type SetUser2 = (name: string, age: number): void;
+// type SetUser2 = (name: string, age: number) => void;
 // 这里没整明白。
+// 2020-10-10 整明白了 类型别名中但函数标签
