aef92e4ed1431a07ddec8b97f050e0e974c6b114
cuishijie <cuishijie@inke.cn>
2020-10-20 10:53:44 +0800

    class.js

---
+++ decorators/class.js
@@ -0,0 +1,27 @@
+// declare type ClassDecorator = <TFunction extends Function>(
+//   target: TFunction
+// ) => TFunction | void;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// 类装饰器顾名思义。它接收一个参数 target: TFuntion - 被装饰类
+// 举个例子
+function Greeter(target) {
+    target.prototype.greet = function () {
+        console.log("Hello Semlinker");
+    };
+}
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+        //
+    }
+    Greeting = __decorate([
+        Greeter
+    ], Greeting);
+    return Greeting;
+}());
+var myGreeting = new Greeting();
+myGreeting.greet(); // console output: "Hello Semlinker"
