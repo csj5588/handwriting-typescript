aaf3f6c406ec5e8cd386cd6a867d54981ef23571
cuishijie <cuishijie@inke.cn>
2021-03-10 11:11:29 +0800

    types vs interface

--- type/type.ts
+++ type/type.ts
@@ -21,3 +21,8 @@ type User2 = {
 // type SetUser2 = (name: string, age: number) => void;
 // 这里没整明白。
 // 2020-10-10 整明白了 类型别名中但函数标签
+// 2021-03-10
+// types vs interface
+// 当创建新当对象及对象方法当时候使用interface
+// 当定义一个函数当时候使用types
+// 他们需要配合的工作并且在项目中不断摸索
